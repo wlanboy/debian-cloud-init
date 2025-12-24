9759308b9368380b2350f3fce7fa22ee2e768a4a
wlanboy <wlanboy@github.com>
2025-12-24 10:06:03 +0100

    removed unsused lines

--- templates/amd64-tools.sh
+++ templates/amd64-tools.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-set -e
 cd ~
-
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo cp ./kubectl /usr/local/bin
